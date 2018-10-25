1f1a0336cad7359fd2b27abf57f069d1c9e4df43
Giovanni Tomasino <36841365+TomaIT@users.noreply.github.com>
2018-10-25 13:16:58 +0200

    Create main.c

---
+++ main/main.c
@@ -0,0 +1,40 @@
+#include "Functions_wrapper.h"
+
+esp_err_t event_handler(void *ctx, system_event_t *event) {
+	return ESP_OK;
+}
+
+
+void app_main(void) {
+	nvs_flash_init();
+	tcpip_adapter_init();
+
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+	ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+	ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+
+	// Init dummy AP to specify a channel and get WiFi hardware into
+	// a mode where we can send the actual fake beacon frames.
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+	wifi_config_t ap_config = {
+		.ap = {
+			.ssid = "esp32-beaconspam",
+			.ssid_len = 0,
+			.password = "dummypassword",
+			.channel = 1,
+			.authmode = WIFI_AUTH_WPA2_PSK,
+			.ssid_hidden = 1,
+			.max_connection = 0,
+			.beacon_interval = 60000
+		}
+	};
+
+	ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &ap_config));
+	ESP_ERROR_CHECK(esp_wifi_start());
+	ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+
+	xTaskCreate(&spam_beacon_task, "spam_task", 2048, NULL, 5, NULL);
+
+}
