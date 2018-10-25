4963e145e8c8c5c1b0dc87379a27975c57d4920d
Giovanni Tomasino <36841365+TomaIT@users.noreply.github.com>
2018-10-25 13:18:40 +0200

    Create Functions_wrapper.h

---
+++ main/Functions_wrapper.h
@@ -0,0 +1,15 @@
+/*
+ * Functions_wrapper.h
+ *
+ *  Created on: 24 ott 2018
+ *      Author: giova
+ */
+
+#ifndef MAIN_FUNCTIONS_WRAPPER_H_
+#define MAIN_FUNCTIONS_WRAPPER_H_
+
+#include "Headers_wrapper.h"
+
+void spam_beacon_task(void *vParameters);
+
+#endif /* MAIN_FUNCTIONS_WRAPPER_H_ */
