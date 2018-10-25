f9fc9b57c1c241f64c572909e5ae1e369534bb5d
Giovanni Tomasino <36841365+TomaIT@users.noreply.github.com>
2018-10-25 13:19:13 +0200

    Create Headers_wrapper.h

---
+++ main/Headers_wrapper.h
@@ -0,0 +1,24 @@
+/*
+ * Headers_wrapper.h
+ *
+ *  Created on: 24 ott 2018
+ *      Author: giova
+ */
+
+#ifndef MAIN_HEADERS_WRAPPER_H_
+#define MAIN_HEADERS_WRAPPER_H_
+
+#include "freertos/FreeRTOS.h"
+
+#include "esp_event_loop.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_wifi.h"
+
+#include "nvs_flash.h"
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+
+#endif /* MAIN_HEADERS_WRAPPER_H_ */
